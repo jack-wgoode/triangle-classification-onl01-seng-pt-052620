5e6359043dac98bb0d1a96eeaf804aecba04c243
Jack Goode <jack.w.goode@icloud.com>
2020-06-30 21:16:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -25,7 +25,7 @@ class Triangle
       end
       
       def inequality? 
-        @side_1 > @side_3 + @side_2 || @side_2 > @side_3 + @side_1  || @side_3 > @side_2 + @side_1 
+        @side_1 >= @side_3 + @side_2 || @side_2 >= @side_3 + @side_1  || @side_3 >= @side_2 + @side_1 
       end 
       
       def side_is_zero?
