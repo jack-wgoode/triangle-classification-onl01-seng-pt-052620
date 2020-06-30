ca0017bc4033f0929ab7f110a02cd4462bad9851
Jack Goode <jack.w.goode@icloud.com>
2020-06-30 20:33:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,68 @@
 class Triangle
-  # write code here
-end
+  
+  
+  
+      def initialize(side_1, side_2, side_3)
+        @side_1 = side_1 
+        @side_2 = side_2 
+        @side_3 = side_3
+      end
+        
+      def equilateral? 
+        @side_1 == @side_2 && @side_2 == @side_3 
+      end
+      
+      def isosceles? 
+        @side_1 == @side_2 || @side_1 == @side_3 || @side_3 == @side_2
+      end
+      
+      def scalene? 
+        @side_1 != @side_2 && @side_2 != @side_3 && @side_3 != @side_1 
+      end
+      
+      def invalid?
+        self.inequality? || self.negative?
+      end
+      
+      def inequality? 
+        @side_1 < @side_3 + @side_2 || @side_2 < @side_3 + @side_1  || @side_3 < @side_2 + @side_1 
+      end 
+      
+      def negative?
+        @side_1 * @side_2 *  @side_3 == 0 
+      end
+      
+      def triangle_type
+        return :equilateral if self.equilateral?
+        return :isosceles if self.isosceles?
+        return :scalene if self.scalene?
+      end
+      
+      def kind 
+       if self.negative?
+      raise TriangleError
+    elsif self.inequality?
+      raise TriangleError
+    else
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
+    end
+     end
+   
+      
+      
+      class TriangleError < StandardError
+        
+        def message 
+          "This is not a valid triangle. The sum of 2 sides of a triangle must be greater than the last side." 
+        end
+        
+      end
+    
+    
+    end
